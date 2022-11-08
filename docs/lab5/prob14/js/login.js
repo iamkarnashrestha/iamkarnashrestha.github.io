bde424eda0d4f0c9440f2dfbac90fcc3fb8bcaf1
caranstha <caranofficial@gmail.com>
2022-11-07 21:52:45 -0600

    lab 5

--- docs/lab5/prob14/js/login.js
+++ docs/lab5/prob14/js/login.js
@@ -1,8 +1,9 @@
-
+//form.addEventListener("submit",showInputsToConsole(),true);
 function showInputsToConsole()
-{
+{   
     console.log("Web Url: "+document.getElementById("webUrl").value);
     console.log("Email: "+document.getElementById("email").value);
     console.log("Password: "+document.getElementById("password").value);
     console.log("CheckBox Input: "+document.getElementById("defaultCheck1").value);
+    
 }
